dd360bc3f874ea8eea3fe2d814be922dbdbacf95
Kjeff24 <jeffreyarthur123@gmail.com>
2023-11-17 21:08:13 +0000

    pull from serializers

---
+++ backend/routers.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from product.viewsets import ProductGenericViewSet
+
+
+router = DefaultRouter()
+router.register('products', ProductGenericViewSet, basename='products')
+
+
+urlpatterns = router.urls
